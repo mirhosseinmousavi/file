67e36a2c44ffd381a02f8d37fea0a326c0af231e
unknown <mirhosseinmousavi42@gmail.com>
2021-10-21 19:58:38 +0330

    'hello'

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 8080
+
+app.post("/", (req, res) => {
+    return res.status(200).json({success: true})
+})
+
+app.listen(port, () => {
+    console.log(`server running at port ${port}`)
+})
